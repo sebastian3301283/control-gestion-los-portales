9b91fbf2694afd0397cd9198de79a02bb7550af1
Sebastian Aguilat <116051904+sebastian3301283@users.noreply.github.com>
2026-09-01 17:21:27 -0500

    Collapse configuration sections on initial load

--- src/CatalogConfiguration.tsx
+++ src/CatalogConfiguration.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import CatalogConfigurationLegacy from './CatalogConfigurationLegacy'
 import GuidelineCatalog from './GuidelineCatalog'
 import PeriodCatalog from './PeriodCatalog'
@@ -6,6 +7,13 @@ type Unit = { code: string; name: string }
 type Props = { units?: Unit[]; canManage: boolean }
 
 export default function CatalogConfiguration(props: Props) {
+  useEffect(() => {
+    const frame = window.requestAnimationFrame(() => {
+      document.querySelectorAll<HTMLButtonElement>('.config-accordion.open > .config-accordion-head').forEach(button => button.click())
+    })
+    return () => window.cancelAnimationFrame(frame)
+  }, [])
+
   return <div style={{ display: 'grid', gap: 16 }}>
     <PeriodCatalog canManage={props.canManage} />
     <CatalogConfigurationLegacy {...props} />
