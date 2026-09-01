4effb7fc43ac3c068be7e06dbbc44a2897139284
Sebastian Aguilat <116051904+sebastian3301283@users.noreply.github.com>
2026-09-01 16:53:57 -0500

    Move period management into configuration

--- src/CatalogConfiguration.tsx
+++ src/CatalogConfiguration.tsx
@@ -1,11 +1,13 @@
 import CatalogConfigurationLegacy from './CatalogConfigurationLegacy'
 import GuidelineCatalog from './GuidelineCatalog'
+import PeriodCatalog from './PeriodCatalog'
 
 type Unit = { code: string; name: string }
 type Props = { units?: Unit[]; canManage: boolean }
 
 export default function CatalogConfiguration(props: Props) {
   return <div style={{ display: 'grid', gap: 16 }}>
+    <PeriodCatalog canManage={props.canManage} />
     <CatalogConfigurationLegacy {...props} />
     <GuidelineCatalog {...props} />
   </div>
