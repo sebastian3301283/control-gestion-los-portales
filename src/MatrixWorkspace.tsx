eaf665473d68c3348657e83a362bcf5876956e96
Sebastian Aguilat <116051904+sebastian3301283@users.noreply.github.com>
2026-09-02 09:03:28 -0500

    Enable Central lineamientos permissions and matrix history

--- src/MatrixWorkspace.tsx
+++ src/MatrixWorkspace.tsx
@@ -1,5 +1,6 @@
-import MatrixWorkspaceV8 from './MatrixWorkspaceV8'
+import MatrixWorkspaceV10 from './MatrixWorkspaceV10'
 import './matrix-workspace-v6.css'
 import './matrix-workspace-v9.css'
+import './matrix-workspace-v10.css'
 
-export default MatrixWorkspaceV8
+export default MatrixWorkspaceV10
