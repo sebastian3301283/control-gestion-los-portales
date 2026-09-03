3dda05cbbf893724b86c78f2b9ba10bd3f8d0fa1
Sebastian Aguilat <116051904+sebastian3301283@users.noreply.github.com>
2026-09-03 12:39:24 -0500

    Type Central matrix hierarchy helper

--- src/matrix-subpoints.d.ts
+++ src/matrix-subpoints.d.ts
@@ -1,2 +1,7 @@
 export function splitSubpoints(value: unknown): string[]
 export function normalizeSubpoints(value: unknown): string
+export function prepareCentralMatrixFields(lineamiento: unknown, objective: unknown, subpoints: unknown): {
+  objective_group: string
+  objective: string
+  action_plan: string
+}
