0fc7755a54448e22a33940ba6cb634deb8c37585
Sebastian Aguilat <116051904+sebastian3301283@users.noreply.github.com>
2026-09-14 15:18:19 -0500

    feat: expose guideline responsible configuration

--- src/CatalogConfiguration.tsx
+++ src/CatalogConfiguration.tsx
@@ -3,6 +3,7 @@ import CatalogConfigurationLegacy from './CatalogConfigurationLegacy'
 import PeriodCatalog from './PeriodCatalog'
 import PermissionCatalogV4 from './PermissionCatalogV4'
 import GuidelineAreaCatalog from './GuidelineAreaCatalog'
+import GuidelineResponsibleConfiguration from './GuidelineResponsibleConfiguration'
 import './configuration-area-filter.css'
 
 type Unit = { code: string; name: string }
@@ -87,6 +88,7 @@ export default function CatalogConfiguration(props: Props) {
 
   return <div ref={rootRef} className="configuration-catalog-stack" style={{ display: 'grid', gap: 16 }}>
     <PeriodCatalog canManage={props.canManage} />
+    <GuidelineResponsibleConfiguration units={props.units} canManage={props.canManage} />
     <CatalogConfigurationLegacy {...props} />
     <GuidelineAreaCatalog {...props} />
     <PermissionCatalogV4 {...props} />
