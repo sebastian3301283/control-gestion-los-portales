5d83ee322ce49d50add7dce348fed3a85a2101da
Sebastian Aguilat <116051904+sebastian3301283@users.noreply.github.com>
2026-09-15 12:39:35 -0500

    style: load platform consistency layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './guideline-visual-fixes.css'
+import './platform-consistency.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />,
